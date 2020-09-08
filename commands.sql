f33e87e90243a3fd723495dae3e6d884bdaf4b04
fernal73 <linus.fernandes@gmail.com>
2020-09-08 16:42:27 +0530

    Update sql

--- commands.sql
+++ commands.sql
@@ -1,3 +1,8 @@
+/* 
+DDL commands to set up tables in
+the database. Using the database test 
+installed with MySQL.
+*/
 
 use test;
 
@@ -6,7 +11,7 @@ Create Table Employee
 (EmpId Integer (10) NOT NULL auto_increment primary key, 
   FirstName Varchar (20) NOT NULL,
   LastName Varchar (20) NOT NULL,
-  MiddleName Varchar (20),
+  MiddleName Varchar (20) default '',
 BirthDate Date NOT NULL,
 Salary Decimal (20, 4) NOT NULL,
 Address Varchar (50) NOT NULL);
@@ -16,14 +21,14 @@ Create Table Student
 (StudentId Integer (10) NOT NULL auto_increment primary key, 
 FirstName Varchar (20) NOT NULL,
   LastName Varchar (20) NOT NULL,
-  MiddleName Varchar (20),
+  MiddleName Varchar (20) default '',
 BirthDate Date,
-State Varchar (20),
+State char (2) Not NULL,
 City Varchar (20));
 
 Drop Table IF EXISTS Course;
 Create Table Course
-(CourseId Varchar (5),
-DepartmentId Varchar (20),
-Title Varchar (20),
-Description Varchar (20));
+(CourseId Char (6) NOT NULL PRIMARY KEY,
+DepartmentId char (6) NOT NULL,
+Title Varchar (20) Not NUll,
+Description Varchar (30) default '');
