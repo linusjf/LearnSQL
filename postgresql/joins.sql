855934171b6a3e29467fc4dad70e8ab09cbf2fc8
fernal73 <linus.fernandes@gmail.com>
2021-05-11 15:00:44 +0530

    Add join statements

--- postgresql/joins.sql
+++ postgresql/joins.sql
@@ -61,3 +61,12 @@ VALUES (
 );
 select * from customers;
 select * from orders;
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
+SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
+FROM Customers A, Customers B
+WHERE A.CustomerID <> B.CustomerID
+AND A.City = B.City
+ORDER BY A.City;
--- postgresql/tutorial.sql
+++ postgresql/tutorial.sql
@@ -101,3 +101,11 @@ SELECT cities.name, weather.temp_lo, weather.temp_hi,
  WHERE cities.abbr = weather.city;
 SELECT *
  FROM weather INNER JOIN cities ON (weather.city = cities.abbr);
+SELECT *
+ FROM weather LEFT OUTER JOIN cities ON (weather.city =
+ cities.abbr);
+ SELECT W1.city, W1.temp_lo AS low, W1.temp_hi AS high,
+ W2.city, W2.temp_lo AS low, W2.temp_hi AS high
+ FROM weather W1, weather W2
+ WHERE W1.temp_lo < W2.temp_lo
+ AND W1.temp_hi > W2.temp_hi;
