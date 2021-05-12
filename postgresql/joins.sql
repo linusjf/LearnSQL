b7df874825b63947f079836ae70c4e391cdc4951
fernal73 <linus.fernandes@gmail.com>
2021-05-12 05:53:13 +0530

    Format

--- postgresql/joins.sql
+++ postgresql/joins.sql
@@ -1,130 +1,219 @@
-CREATE TABLE IF NOT EXISTS CUSTOMERS (
-CustomerID serial primary key,
-CustomerName varchar(100) not NULL,
-ContactName varchar(100) NOT NULL,
-Address varchar(100) not NULL,
-City varchar(30) NOT NULL,
-PostalCode char(10) NOT NULL,
-Country varchar(50) NOT NULL,
-createDt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updateDt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+create table if not exists CUSTOMERS (
+  CUSTOMERID serial primary key,
+  CUSTOMERNAME VARCHAR(100) not null,
+  CONTACTNAME VARCHAR(100) not null,
+  ADDRESS VARCHAR(100) not null,
+  CITY VARCHAR(30) not null,
+  POSTALCODE CHAR(10) not null,
+  COUNTRY VARCHAR(50) not null,
+  CREATEDT timestamp not null default current_timestamp,
+  UPDATEDT timestamp not null default current_timestamp
 );
-TRUNCATE TABLE CUSTOMERS
-RESTART IDENTITY
-CASCADE;
-CREATE TABLE IF NOT EXISTS ORDERS (
-OrderID bigserial primary key,
-CustomerID integer NOT NULL,
-EmployeeID integer NOT NULL,
-OrderDate date NOT NULL,
-ShipperID integer NOT NULL,
-createDt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updateDt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+truncate table CUSTOMERS restart identity cascade;
+create table if not exists ORDERS (
+  ORDERID bigserial primary key,
+  CUSTOMERID integer not null,
+  EMPLOYEEID integer not null,
+  ORDERDATE date not null,
+  SHIPPERID integer not null,
+  CREATEDT timestamp not null default current_timestamp,
+  UPDATEDT timestamp not null default current_timestamp
 );
-TRUNCATE TABLE ORDERS
-RESTART IDENTITY
-CASCADE;
-CREATE TABLE IF NOT EXISTS SUPPLIERS (
-SupplierID serial primary key,
-SupplierName varchar(100) not NULL,
-ContactName varchar(100) NOT NULL,
-Address varchar(100) not NULL,
-City varchar(30) NOT NULL,
-PostalCode char(10) NOT NULL,
-Country varchar(50) NOT NULL,
-createDt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updateDt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+truncate table ORDERS restart identity cascade;
+create table if not exists SUPPLIERS (
+  SUPPLIERID serial primary key,
+  SUPPLIERNAME VARCHAR(100) not null,
+  CONTACTNAME VARCHAR(100) not null,
+  ADDRESS VARCHAR(100) not null,
+  CITY VARCHAR(30) not null,
+  POSTALCODE CHAR(10) not null,
+  COUNTRY VARCHAR(50) not null,
+  CREATEDT timestamp not null default current_timestamp,
+  UPDATEDT timestamp not null default current_timestamp
 );
-TRUNCATE TABLE SUPPLIERS
-RESTART IDENTITY
-CASCADE;
-INSERT INTO CUSTOMERS (CustomerName,ContactName,Address, City,PostalCode,
-Country)
-VALUES ('Alfreds Futterkiste',
-'Maria Anders',
-'Obere Str. 57',
-'Berlin',
-'12209',
-'Germany'
-),('Ana Trujillo Emparedados y helados',
-'Ana Trujillo',
-'Avda. de la Constitución 2222',
-'México D.F.',
-'05021',
-'Mexico'
-),('Antonio Moreno Taquería',
-'Antonio Moreno',
-'Mataderos 2312',
-'México D.F.',
-'05023',
-'Mexico'
-);
-INSERT INTO ORDERS (
-  CustomerID,
-  EmployeeID,
-  OrderDate,
-  ShipperID)
-VALUES (
-2,7,'1996-09-08',3
-),
- (
-37,3,'1996-09-09',1
-),
- (
-77,8,'1996-09-10',2
-);
-INSERT INTO SUPPLIERS (SupplierName,ContactName,Address, City,PostalCode,
-Country)
-VALUES ('Exotic Liquid',
-'Charlotte Cooper',
-'49, Gilbert Street',
-'London',
-'EC1 4SD',
-'UK'
-),('New Orleans Cajun Delight',
-'Shelley Burke',
-'P.O. Box 78934',
-'New Orleans',
-'70117',
-'USA'),
-('Grandma Kelly''s Homestead',
-  'Regina Murphy',
-  '707 Oxford Road',
-'Ann Arbor',
-'05023',
-'USA'
-);
-select * from customers;
-select * from orders;
-SELECT Customers.CustomerName, Orders.OrderID
-FROM Customers
-FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
-ORDER BY Customers.CustomerName;
-SELECT A.CustomerName AS CustomerName1, B.CustomerName AS CustomerName2, A.City
-FROM Customers A, Customers B
-WHERE A.CustomerID <> B.CustomerID
-AND A.City = B.City
-ORDER BY A.City;
-SELECT City FROM Customers UNION
-SELECT City FROM Suppliers
-ORDER BY City;
-SELECT City FROM Customers UNION ALL
-SELECT City FROM Suppliers
-ORDER BY City;
-SELECT City, Country FROM Customers
-WHERE Country='Germany'
-UNION
-SELECT City, Country FROM Suppliers
-WHERE Country='Germany'
-ORDER BY City;
-SELECT City, Country FROM Customers
-WHERE Country='Germany'
-UNION ALL
-SELECT City, Country FROM Suppliers
-WHERE Country='Germany'
-ORDER BY City;
-SELECT 'Customer' AS Type, ContactName, City, Country
-FROM Customers
-UNION
-SELECT 'Supplier', ContactName, City, Country
-FROM Suppliers;
+truncate table SUPPLIERS restart identity cascade;
+insert into
+  CUSTOMERS (
+    CUSTOMERNAME,
+    CONTACTNAME,
+    ADDRESS,
+    CITY,
+    POSTALCODE,
+    COUNTRY
+  )
+values
+  (
+    'Alfreds Futterkiste',
+    'Maria Anders',
+    'Obere Str. 57',
+    'Berlin',
+    '12209',
+    'Germany'
+  ),
+  (
+    'Ana Trujillo Emparedados y helados',
+    'Ana Trujillo',
+    'Avda. de la Constitución 2222',
+    'México D.F.',
+    '05021',
+    'Mexico'
+  ),
+  (
+    'Antonio Moreno Taquería',
+    'Antonio Moreno',
+    'Mataderos 2312',
+    'México D.F.',
+    '05023',
+    'Mexico'
+  );
+insert into
+  ORDERS (
+    CUSTOMERID,
+    EMPLOYEEID,
+    ORDERDATE,
+    SHIPPERID
+  )
+values
+  (2, 7, '1996-09-08', 3),
+  (37, 3, '1996-09-09', 1),
+  (77, 8, '1996-09-10', 2);
+insert into
+  SUPPLIERS (
+    SUPPLIERNAME,
+    CONTACTNAME,
+    ADDRESS,
+    CITY,
+    POSTALCODE,
+    COUNTRY
+  )
+values
+  (
+    'Exotic Liquid',
+    'Charlotte Cooper',
+    '49, Gilbert Street',
+    'London',
+    'EC1 4SD',
+    'UK'
+  ),
+  (
+    'New Orleans Cajun Delight',
+    'Shelley Burke',
+    'P.O. Box 78934',
+    'New Orleans',
+    '70117',
+    'USA'
+  ),
+  (
+    'Grandma Kelly''s Homestead',
+    'Regina Murphy',
+    '707 Oxford Road',
+    'Ann Arbor',
+    '05023',
+    'USA'
+  );
+select
+  *
+from
+  CUSTOMERS;
+select
+  *
+from
+  ORDERS;
+select
+  CUSTOMERS.CUSTOMERNAME,
+  ORDERS.ORDERID
+from
+  CUSTOMERS full
+  outer join ORDERS on CUSTOMERS.CUSTOMERID = ORDERS.CUSTOMERID
+order by
+  CUSTOMERS.CUSTOMERNAME;
+select
+  A.CUSTOMERNAME as CUSTOMERNAME1,
+  B.CUSTOMERNAME as CUSTOMERNAME2,
+  A.CITY
+from
+  CUSTOMERS A,
+  CUSTOMERS B
+where
+  A.CUSTOMERID <> B.CUSTOMERID
+  and A.CITY = B.CITY
+order by
+  A.CITY;
+select
+  CITY
+from
+  CUSTOMERS
+union
+select
+  CITY
+from
+  SUPPLIERS
+order by
+  CITY;
+select
+  CITY
+from
+  CUSTOMERS
+union all
+select
+  CITY
+from
+  SUPPLIERS
+order by
+  CITY;
+select
+  CITY,
+  COUNTRY
+from
+  CUSTOMERS
+where
+  COUNTRY = 'Germany'
+union
+select
+  CITY,
+  COUNTRY
+from
+  SUPPLIERS
+where
+  COUNTRY = 'Germany'
+order by
+  CITY;
+select
+  CITY,
+  COUNTRY
+from
+  CUSTOMERS
+where
+  COUNTRY = 'Germany'
+union all
+select
+  CITY,
+  COUNTRY
+from
+  SUPPLIERS
+where
+  COUNTRY = 'Germany'
+order by
+  CITY;
+select
+  'Customer' as type,
+  CONTACTNAME,
+  CITY,
+  COUNTRY
+from
+  CUSTOMERS
+union
+select
+  'Supplier',
+  CONTACTNAME,
+  CITY,
+  COUNTRY
+from
+  SUPPLIERS;
+select
+  ORDERS.ORDERID,
+  CUSTOMERS.CUSTOMERNAME,
+  ORDERS.ORDERDATE
+from
+  ORDERS
+  inner join CUSTOMERS on ORDERS.CUSTOMERID = CUSTOMERS.CUSTOMERID;
