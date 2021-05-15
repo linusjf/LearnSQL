71926492b2a6d10ee25d78a41d7d966741963689
fernal73 <linus.fernandes@gmail.com>
2021-05-15 10:39:05 +0530

    Update windows.sql for more windows functions

--- postgresql/windows.sql
+++ postgresql/windows.sql
@@ -10,19 +10,29 @@ CREATE TABLE IF NOT EXISTS empsalary (
   updatedt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 TRUNCATE TABLE empsalary CASCADE;
+ALTER TABLE
+  empsalary
+ADD
+  enroll_date DATE NOT NULL;
 INSERT INTO
-  empsalary (depname, empno, salary, rank)
+  empsalary (
+    depname,
+    empno,
+    salary,
+    rank,
+    enroll_date
+  )
 VALUES
-  ('develop', 8, 6000, 1),
-  ('develop', 10, 5200, 2),
-  ('develop', 11, 5200, 2),
-  ('develop', 9, 4500, 4),
-  ('develop', 7, 4200, 5),
-  ('personnel', 2, 3900, 1),
-  ('personnel', 5, 3500, 2),
-  ('sales', 1, 5000, 1),
-  ('sales', 4, 4800, 2),
-  ('sales', 3, 4800, 2);
+  ('develop', 8, 6000, 1, '1999-01-21'),
+  ('develop', 10, 5200, 2, '2001-03-26'),
+  ('develop', 11, 5200, 2, '2000-12-01'),
+  ('develop', 9, 4500, 4, '2003-06-01'),
+  ('develop', 7, 4200, 5, '2005-01-23'),
+  ('personnel', 2, 3900, 1, '2007-12-23'),
+  ('personnel', 5, 3500, 2, '2005-11-23'),
+  ('sales', 1, 5000, 1, '2000-12-12'),
+  ('sales', 4, 4800, 2, '2004-11-04'),
+  ('sales', 3, 4800, 2, '2005-05-12');
 SELECT
   depname,
   empno,
@@ -54,3 +64,26 @@ SELECT
   )
 FROM
   empsalary;
+SELECT
+  depname,
+  empno,
+  salary,
+  enroll_date
+FROM
+  (
+    SELECT
+      depname,
+      empno,
+      salary,
+      enroll_date,
+      rank() OVER (
+        PARTITION BY depname
+        ORDER BY
+          salary DESC,
+          empno
+      ) AS pos
+    FROM
+      empsalary
+  ) AS ss
+WHERE
+  pos < 3;
