ae73d77de1ae8da6fbb8f9098e172946bd96011f
fernal73 <linus.fernandes@gmail.com>
2021-05-20 15:03:36 +0530

    Add select

--- postgresql/functions.sql
+++ postgresql/functions.sql
@@ -4,3 +4,8 @@ AS $$ SELECT * FROM customers WHERE city = $1 $$
 LANGUAGE SQL;
 select customername from customers_from_city('Berlin');
 select customername from customers_from_city('México D.F.');
+select sqrt(2);
+SELECT
+ count(*) AS unfiltered,
+ count(*) FILTER (WHERE i < 5) AS filtered
+FROM generate_series(1,10) AS s(i);
