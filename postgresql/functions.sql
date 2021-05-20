6443fdde1e1f1efc09385ccdf92d8289209c86f8
fernal73 <linus.fernandes@gmail.com>
2021-05-20 13:31:29 +0530

    Add select using function

--- postgresql/functions.sql
+++ postgresql/functions.sql
@@ -2,3 +2,5 @@ CREATE OR REPLACE FUNCTION customers_from_city(text)
 returns setof customers 
 AS $$ SELECT * FROM customers WHERE city = $1 $$
 LANGUAGE SQL;
+select customername from customers_from_city('Berlin');
+select customername from customers_from_city('México D.F.');
