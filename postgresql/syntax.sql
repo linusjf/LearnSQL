af9267ef132ea9419f9f520cbba541a36efd1d59
fernal73 <linus.fernandes@gmail.com>
2021-05-20 12:36:16 +0530

    Add syntax

---
+++ postgresql/syntax.sql
@@ -0,0 +1,6 @@
+SELECT 3 OPERATOR(pg_catalog.+) 4;
+SELECT 3 OPERATOR(pg_catalog.-) 4;
+SELECT 3 OPERATOR(pg_catalog.*) 4;
+SELECT 3 OPERATOR(pg_catalog./) 4;
+SELECT 3 OPERATOR(pg_catalog.%) 4;
+SELECT 3 OPERATOR(pg_catalog.^) 4;
