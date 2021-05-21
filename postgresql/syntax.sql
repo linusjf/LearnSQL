8b1c620f49bb7b28327fe576bcf6a3bc27f09b76
fernal73 <linus.fernandes@gmail.com>
2021-05-21 19:45:19 +0530

    add array sql

--- postgresql/syntax.sql
+++ postgresql/syntax.sql
@@ -10,3 +10,13 @@ SELECT
   3 operator(pg_catalog.%) 4;
 SELECT
   3 operator(pg_catalog.^) 4;
+SELECT ARRAY[1,2,3+4];
+SELECT ARRAY[1,2,22.7]::integer[];
+SELECT ARRAY[ARRAY[1,2], ARRAY[3,4]];
+SELECT ARRAY[[1,2],[3,4]];
+CREATE TABLE arr(f1 int[], f2 int[]);
+INSERT INTO arr VALUES (ARRAY[[1,2],[3,4]], ARRAY[[5,6],[7,8]]);
+SELECT ARRAY[f1, f2, '{{9,10},{11,12}}'::int[]] FROM arr;
+SELECT ARRAY[]::integer[];
+SELECT ARRAY(SELECT oid FROM pg_proc WHERE proname LIKE 'bytea%');
+SELECT ARRAY(SELECT ARRAY[i, i*2] FROM generate_series(1,5) AS a(i));
